73172856431e0efca13f607d410a5cfda9915f4a
Ilan Diamond <ilan.d@next-insurance.com>
2021-05-18 13:59:53 +0300

    Change colors 2

--- pages/index.js
+++ pages/index.js
@@ -5,9 +5,9 @@ export default function Home({ isTestVariant }) {
   return (
     <div className={styles.container}>
       {isTestVariant ? (
-        <h1 style={{ color: '#0070f3', fontSize: 82 }}>Control</h1>
-      ) : (
         <h1 style={{ color: '#ffdf59', fontSize: 82 }}>Test</h1>
+      ) : (
+        <h1 style={{ color: '#0070f3', fontSize: 82 }}>Control</h1>
       )}
     </div>
   );
