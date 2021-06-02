f55acc8e32983cd5f5b72681ccccc400a6aabadd
Ilan Diamond <ilan.d@next-insurance.com>
2021-06-02 12:14:48 +0300

    Change var names

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,9 +1,9 @@
 import styles from '../../styles/Home.module.css';
 
-export default function Blog({ isExperiment }) {
+export default function Blog({ testVariantId }) {
   return (
     <div className={styles.container}>
-      {isExperiment ? (
+      {testVariantId === "1" ? (
         <h1 style={{ color: '#ffdf59', fontSize: 82 }}>Test</h1>
       ) : (
         <h1 style={{ color: '#0070f3', fontSize: 82 }}>Control</h1>
@@ -13,10 +13,10 @@ export default function Blog({ isExperiment }) {
 }
 
 export async function getServerSideProps({ req }) {
-  const {'is-experiment': isExperiment} = req.headers;
+  const {'test-variant-id': testVariantId} = req.headers;
   return {
     props: {
-      isExperiment: isExperiment ? isExperiment : null,
+      testVariantId: testVariantId ? testVariantId : null,
     },
   };
 }
