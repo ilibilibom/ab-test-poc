d1cf553c24d629ec9c6d268a79ec4fd4b998eafb
Ilan Diamond <ilan.d@next-insurance.com>
2021-06-02 12:29:54 +0300

    Push me

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -3,7 +3,7 @@ import styles from '../../styles/Home.module.css';
 export default function Blog({ testVariantId }) {
   return (
     <div className={styles.container}>
-      {testVariantId === "1" ? (
+      {testVariantId === '1' ? (
         <h1 style={{ color: '#ffdf59', fontSize: 82 }}>Test</h1>
       ) : (
         <h1 style={{ color: '#0070f3', fontSize: 82 }}>Control</h1>
