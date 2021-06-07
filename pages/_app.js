a7d0c413e5d37eaaa892bcf1c0f1f8cd0ab99c16
Ilan Diamond <ilan.d@next-insurance.com>
2021-06-08 00:23:42 +0300

    come on !!!!

--- pages/_app.js
+++ pages/_app.js
@@ -4,4 +4,15 @@ function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
 
+MyApp.getInitialProps = async (appContext) => {
+  // const [appProps, mainMenuItems, mainFooterData] =
+  //   await Promise.all([
+  //       App.getInitialProps(appContext),
+  //       getTopMenuData(),
+  //       getFooterData()
+  //     ]);
+  console.log('appContext',appContext);
+  return {};
+};
+
 export default MyApp
