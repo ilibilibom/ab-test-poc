74ed975dcc41f03838cdd81c0295f2aee266335b
Ilan Diamond <ilan.d@next-insurance.com>
2021-06-16 14:20:59 +0300

    Go Go Go

--- pages/blog/[[...slug]].js
+++ pages/blog/[[...slug]].js
@@ -1,36 +1,18 @@
 import styles from '../../styles/Home.module.css';
 
 export default function Blog({slug}) {
+  const backColor = getRandomColor();
   return (
     <div className={styles.container}>
-      <div style={{ color: 'white', background: getRandomColor(), fontSize: 25, padding: 50 }}>
-        <pre>{JSON.stringify(slug, undefined, 2)}</pre>
+      <div style={{ color: 'white', background: backColor, fontSize: 25, padding: 50 }}>
+        <pre>{slug}</pre>
       </div>
     </div>
   );
 }
 
-export async function getStaticPaths() {
-  const slugs = ['post-1', 'post-2', 'post-3', 'post-4', 'post-5', 'blog'];
-  let paths = slugs.map((url) => ({ params: { slug: [url] } }));
-
-  const json = 'https://ab-tests-bucket.s3.amazonaws.com/ab-tests/ab-tests.json';
-  const AbTestData = await fetch(json).then((res) => res.json());
-  const testPages = AbTestData.filter(({ testUrl }) => slugs.includes(testUrl));
-
-  testPages.forEach(({testUrl, userGroups}) => {
-    userGroups.forEach(({variantId}) => {
-      paths.push({ params: { slug: [`${testUrl}/${variantId}`] } })
-    })
-  });
-
-  return {
-    paths,
-    fallback: false,
-  };
-}
-
-export async function getStaticProps({ params }) {
+export async function getServerSideProps( { params, req: {headers} } ) {
+  console.log('headers',headers);
   const { slug } = params || '';
     return {
     props: {
